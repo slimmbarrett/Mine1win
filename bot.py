b7d534959e759b8c140c21aba7d82446a855edcb
slimmbarrett <73174484+slimmbarrett@users.noreply.github.com>
2024-11-21 18:11:56 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,16 @@
 import os
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo, Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
+import logging
 
-# Загрузка токена из переменной окружения для безопасности
-BOT_TOKEN = os.getenv('7537994303:AAG15uJYwAmzVINLchNmzIjZ7So95RdkpdI')
+# Настройка логирования
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+# Ваш токен API
+BOT_TOKEN = '7537994303:AAG15uJYwAmzVINLchNmzIjZ7So95RdkpdI'
 
 # URL вашего Web App
 WEB_APP_URL = 'https://mine1win.vercel.app/'
@@ -12,7 +19,7 @@ def start(update: Update, context: CallbackContext) -> None:
     """Обработчик команды /start"""
     # Кнопка с веб-приложением
     keyboard = [
-        [InlineKeyboardButton("Начать игру", web_app={"url": WEB_APP_URL})]
+        [InlineKeyboardButton("Начать игру", web_app=WebAppInfo(url=WEB_APP_URL))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
@@ -24,9 +31,9 @@ def start(update: Update, context: CallbackContext) -> None:
 
 def main() -> None:
     """Основная функция для запуска бота"""
-    # Получаем токен из переменной окружения
-    if BOT_TOKEN is None:
-        print("Ошибка: Не указан BOT_TOKEN в переменных окружения!")
+    # Проверяем наличие токена
+    if not BOT_TOKEN:
+        logging.error("Ошибка: Токен бота не указан!")
         return
 
     # Создаем экземпляр бота и передаем токен
@@ -43,7 +50,7 @@ def main() -> None:
         updater.start_polling()
         updater.idle()
     except Exception as e:
-        print(f"Ошибка при запуске бота: {e}")
+        logging.error(f"Ошибка при запуске бота: {e}")
 
 if __name__ == '__main__':
     main()
