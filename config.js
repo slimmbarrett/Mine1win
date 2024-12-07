ceb66f4bbfa4bd5a85257bb736ce8b835dc07e81
slimmbarrett <73174484+slimmbarrett@users.noreply.github.com>
2024-12-07 15:20:10 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    BOT_TOKEN: '7534371511:AAHtUxfYH1DlPwhvwgX4smqjY2V3BWkDblc',
+    CHANNEL_USERNAME: 'https://t.me/+-OM0Pl6Ow_I2ZmM0',
+    TELEGRAM_API_URL: 'https://api.telegram.org'
+};
+
+export default config;
